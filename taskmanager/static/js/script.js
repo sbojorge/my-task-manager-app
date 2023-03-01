447071279d1bad3580a1f35e7b5159ffe42e81c1
sbojorge <sara.bojorge@gmail.com>
2023-03-01 09:54:46 +0000

    Fix typos in the add task form

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,4 +13,4 @@ document.addEventListener('DOMContentLoaded', function() {
     // select initialization
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
-  });
+});
