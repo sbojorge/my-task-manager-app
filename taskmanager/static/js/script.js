3959c988d350083ede7b72b5eab9fcae54c864bc
sbojorge <sara.bojorge@gmail.com>
2023-03-01 09:37:29 +0000

    Create the form for adding new tasks in the add_task.html

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // sidebar initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmm, yyyy",
+      i18n: {done:"Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
